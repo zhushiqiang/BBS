851f77693a1f31832a2dbe419c0439bce4de6baa
vagrant <vagrant@homestead>
2017-11-18 13:27:18 +0000

    Move user model to models folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yirld('title', 'WE199bbs')</title>
+    <title>@yield('title', 'WE199bbs')</title>
 
     <!-- Style -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
