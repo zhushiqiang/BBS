a50c866c1337ef0cb8f12bb713daec170db98400
vagrant <vagrant@homestead>
2017-11-19 14:02:06 +0000

    完成用户添加话题

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,18 @@
 <?php
-
+/**
+ * @return mixed
+ */
 function route_class(){
     return str_replace('.','-',Route::currentRouteName());
+}
+
+/**
+ * @param $value
+ * @param int $length
+ * @return string
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
