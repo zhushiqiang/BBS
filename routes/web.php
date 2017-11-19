e47c54389508f93ac18e87634cf9ab3d984f6bf0
vagrant <vagrant@homestead>
2017-11-19 08:12:14 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Auth::routes();
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
