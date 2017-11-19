a2407aa2c2dc3423efe42f74c2a3103e8435ecf0
vagrant <vagrant@homestead>
2017-11-19 05:56:46 +0000

    Create categories

---
+++ database/migrations/2017_11_19_133219_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index()->commit('名称');
+            $table->text('description')->nullable()->commit('描述');
+            $table->integer('post_count')->default(0)->commit('帖子数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
