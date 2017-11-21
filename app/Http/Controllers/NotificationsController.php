7453245e3011d099f5ff33e0861a4f2451f45076
Juuso <1175482367@QQ.com>
2017-11-21 11:55:21 +0000

    完成消息通知

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class NotificationsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index(){
+        $notifications = Auth::user()->notifications()->paginate(20);
+        Auth::user()->markAsRead();
+        return view('notifications.index', compact('notifications'));
+    }
+}
